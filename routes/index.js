1f14af2c6880aebe0cabfe3ae46f144e91a1942b
VaibhavN007 <vaibhavnit007@gmail.com>
2017-05-09 12:42:25 +0530

    finish process/* route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,25 @@ router.get('/', function(req, res, next) {
 	res.render('index', { title: 'Express' });
 });
 
+router.all('/process/*', function(req, res) {
+	
+	var responseObj = {
+		time: new Date(),
+		method: req.method,
+		headers: req.headers,
+		path: req.originalUrl,
+		query: req.query,
+		body: req.body,
+		duration: getRandomInt(15, 30)
+	};
+
+	res.status(200).json(responseObj);
+});
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
 module.exports = router;
